1e0158b13441098ed1b2a5519d543294ea4b19bf
Joaquín González <joaquin4525@hotmail.com>
2021-04-26 21:10:27 -0300

    Add project files.

---
+++ Grupo2.eCommerceMVC.Entities/Localidad.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eCommerce.Data.Models
+{
+    public class Localidad
+    {
+        public int idLocalidad { get; set; }
+
+        public int idProvincia { get; set; }
+
+        public string nombre { get; set; }
+    }
+}
---
+++ Grupo2.eCommerceMVC.Entities/Usuario.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eCommerce.Data.Models
+{
+    public class Usuario
+    {
+        public int idUsuario { get; set; }
+
+        public string usuario { get; set; }
+
+        public string password { get; set; }
+
+        public string email { get; set; }
+
+        public int bloqueo { get; set; }
+
+        public int admin { get; set; }
+    }
+}
